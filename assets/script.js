2775b3262e1863b98e2af9c3e00a1e9aefd9f9a0
Shanzzz <shahnazafreen9849@gmail.com>
2023-02-01 15:29:25 -0500

    “Done

--- assets/script.js
+++ assets/script.js
@@ -15,3 +15,11 @@ if (topic === 'HTML') {
    } else {
     console.log('Please try again!');
    }
+
+   var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
+console.log('Here are the topics we learned through Prework:');
+listTopics();
+
+console.log('Which topic should we study first?');
+selectTopic();
